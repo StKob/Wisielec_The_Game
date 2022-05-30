275668eb52a1514632bb2a9a8f296bbd197db5ac
StKob <203904@edu.p.lodz.pl>
2022-05-30 15:27:43 +0200

    Wyświetlanie głównego okna oraz pętla w main do działania programu

---
+++ Screen.py
@@ -0,0 +1,11 @@
+﻿import pygame
+
+
+class Screen:
+
+    def __init__(self):
+        pygame.init()
+        info_object = pygame.display.Info()
+        pygame.display.set_mode((info_object.current_h * 0.7, info_object.current_h * 0.7))
+
+
--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import pygame
+
+from Screen import Screen
 from Words import WordlistManager
+
 
 def print_hi(name):
     print(f'Hi, {name}')
+
+
+def working_loop():
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
 
 if __name__ == '__main__':
@@ -9,4 +22,6 @@ if __name__ == '__main__':
     file = WordlistManager("Wordlist.txt")
     file.read_file()
     file.print_dict()
+    scr = Screen()
+    working_loop()
 
