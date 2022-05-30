a3c0f31ad436a12803a4acf587e3ff449a838a8f
StKob <203904@edu.p.lodz.pl>
2022-05-30 15:35:27 +0200

    Tło głównego okna ustawione na kolor biały

--- Screen.py
+++ Screen.py
@@ -6,6 +6,8 @@ class Screen:
     def __init__(self):
         pygame.init()
         info_object = pygame.display.Info()
-        pygame.display.set_mode((info_object.current_h * 0.7, info_object.current_h * 0.7))
+        disp = pygame.display.set_mode((info_object.current_w * 0.7, info_object.current_h * 0.7))
+        disp.fill((255, 255, 255))
+        pygame.display.update()
 
 
