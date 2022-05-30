619b374db5709031befe500c5332eae3a34433c9
Gabriela Niemyjska <235931@edu.p.lodz.pl>
2022-05-30 15:35:37 +0200

    próba utworzenie okna startowego

---
+++ Screen_start.py
@@ -0,0 +1,18 @@
+﻿import pygame.display
+
+
+class Screen_start:
+
+    def __init__(self):
+        self.name = "Okno startowe"
+        win = pygame.display.set_mode((500,500))
+        pygame.display.set_caption(self.name)
+
+
+    def run(self):
+        active = True
+        while active:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    active = False
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from Screen_start import Screen_start
 from Words import WordlistManager
-
+import pygame
 def print_hi(name):
     print(f'Hi, {name}')
 
@@ -8,5 +9,8 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     file = WordlistManager("Wordlist.txt")
     file.read_file()
-    file.print_dict()
+    #file.print_dict()
+    pygame.init()
+    screen_start = Screen_start()
+    screen_start.run()
 
